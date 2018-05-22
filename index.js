2a1c3016528cb3745dc245146e820b7c11886e39
Gareth Lee <clee206@myseneca.ca>
2018-05-22 08:22:16 -0400

    new

--- index.js
+++ index.js
@@ -9,15 +9,27 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-// setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
-   res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
-});
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-// setup another route to listen on /about
-app.get("/about", function(req,res){
-   res.sendFile(path.join(__dirname, "/views/about.html"));
-});
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// // setup a 'route' to listen on the default url path (http://localhost)
+// app.get("/", function(req,res){
+//    res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
+// });
+
+// // setup another route to listen on /about
+// app.get("/about", function(req,res){
+//    res.sendFile(path.join(__dirname, "/views/about.ejs"));
+// });
+
+// set the homepage route
+app.get('/', function(req, res) {
+  // ejs render automatically looks in the views folder
+  res.render('about');
+})
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
